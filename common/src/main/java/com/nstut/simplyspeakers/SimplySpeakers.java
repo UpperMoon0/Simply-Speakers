fda40142d22ce0d1807a47e46bfaeb6d919cafbd
UpperMoon0 <60876500+UpperMoon0@users.noreply.github.com>
2025-05-16 03:05:02 +0700

    refactor: Clean up imports and improve creative tab registration in SimplySpeakers

--- common/src/main/java/com/nstut/simplyspeakers/SimplySpeakers.java
+++ common/src/main/java/com/nstut/simplyspeakers/SimplySpeakers.java
@@ -7,7 +7,6 @@ import com.nstut.simplyspeakers.platform.Services;
 import com.nstut.simplyspeakers.network.PacketRegistries;
 import com.nstut.simplyspeakers.client.ClientEvents;
 import dev.architectury.registry.registries.DeferredRegister;
-import dev.architectury.registry.registries.Registrar;
 import dev.architectury.registry.registries.RegistrySupplier;
 import dev.architectury.platform.Platform;
 import net.minecraft.core.registries.Registries;
@@ -26,7 +25,7 @@ public class SimplySpeakers {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "simplyspeakers";
     // Directly reference a slf4j logger
-    public static final Logger LOGGER = LoggerFactory.getLogger("Simply Speakers");    // Create registry instances for our mod's content
+    public static final Logger LOGGER = LoggerFactory.getLogger("Simply Speakers");   
     public static final DeferredRegister<SoundEvent> SOUND_EVENTS = DeferredRegister.create(MOD_ID, Registries.SOUND_EVENT);
     public static final DeferredRegister<CreativeModeTab> CREATIVE_TABS = DeferredRegister.create(MOD_ID, Registries.CREATIVE_MODE_TAB);
 
@@ -34,10 +33,9 @@ public class SimplySpeakers {
     public static final ResourceLocation MUSIC_SOUND_ID = new ResourceLocation(MOD_ID, "custom.music");
     public static final RegistrySupplier<SoundEvent> MUSIC = SOUND_EVENTS.register("custom.music",
             () -> SoundEvent.createVariableRangeEvent(MUSIC_SOUND_ID));
-    
-    // Creative tab for our items
+
     public static final RegistrySupplier<CreativeModeTab> CREATIVE_TAB = CREATIVE_TABS.register("tab", () ->
-            CreativeModeTab.builder()
+            CreativeModeTab.builder(CreativeModeTab.Row.TOP, 0)  // Adding Row.TOP and position 0
                     .title(Component.translatable("itemGroup." + MOD_ID + ".tab"))
                     .icon(() -> new ItemStack(Services.ITEMS.getSpeakerItem()))
                     .displayItems((parameters, output) -> {
@@ -51,11 +49,12 @@ public class SimplySpeakers {
 
     /**
      * Initializes the mod.
-     */
+     */    
     public static void init() {
         LOGGER.info("Initializing Simply Speakers");
           // Register all of our DeferredRegisters
         SOUND_EVENTS.register();
+        CREATIVE_TABS.register(); // Register the creative tab
         BlockRegistries.init();
         BlockEntityRegistries.init();
         ItemRegistries.init();
